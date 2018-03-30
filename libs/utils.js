b70e9d87ea5012da0d694fa955b91157677dcded
DezkaReid <elmaildeldezkareid@gmail.com>
2018-03-30 03:04:58 -0600

    added methods to filter arguments a process data

---
+++ libs/utils.js
@@ -0,0 +1,47 @@
+const propertiesPresentInObject = (object, ...properties) =>
+  properties.filter(property => typeof object[property] !== 'undefined')
+
+const getPropertiesFromObject = (object, ...properties) =>
+  properties.map(property => object[property])
+  .filter(property => typeof property !== 'undefined')
+
+function selectKeysFromObject (object, ...keys) {
+  const copyObject = {}
+  keys.forEach(key => {
+    copyObject[key] = object[key]
+  })
+  return copyObject
+}
+function writeTemplate (templatePath, destinationPath, data) {
+  this.fs.copyTpl(
+    this.templatePath(templatePath),
+    this.destinationPath(destinationPath),
+    data
+  )
+}
+
+function objectToMap (object) {
+  const map = new Map()
+  for (let key in object) {
+    map.set(key, object[key])
+  }
+  return map
+}
+
+function mapKeysToArray (map, ...keys) {
+  return keys.map(key => map.get(key))
+}
+
+function leftJoinArrays (arrayA, arrayB) {
+  return arrayA.filter((element) => arrayB.indexOf(element) === -1)
+}
+
+module.exports = {
+  getPropertiesFromObject,
+  leftJoinArrays,
+  mapKeysToArray,
+  objectToMap,
+  propertiesPresentInObject,
+  selectKeysFromObject,
+  writeTemplate
+}
